1b783bb820d138fdfc110dce8dc95c485cb4e1ea
Seldo <github@seldo.com>
2013-08-23 18:49:25 -0400

    Datasource loading

--- datasources.js
+++ datasources.js
@@ -2,6 +2,16 @@ var fs = require('fs-extra')
 
 exports.load = function(cb) {
   fs.readdir("./datasources/",function(er,files) {
-    cb()
+    var loaded = {}
+    var count = files.length
+    if (count == 0) cb(loaded)
+    var complete = function() {
+      count--
+      if (count==0) cb(loaded)
+    }
+    files.forEach(function(file) {
+      loaded[file] = require('./datasources/'+file)
+      loaded[file].initialize(complete)
+    })
   })
 }
